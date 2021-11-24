fe46dfec0476b67e2581881c92acbc934f95d45d
YafietWeldegabir <yafietweldegabir@gmail.com>
2021-11-23 21:11:22 -0600

    innit

---
+++ order-service/src/test/java/yafiet/net/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package yafiet.net.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
