54da3909349146c829f67a27d1a96d1faa307502
YafietWeldegabir <yafietweldegabir@gmail.com>
2021-11-25 16:24:35 -0600

    innit config server

--- configuaration-server/src/main/java/yafiet/net/configuarationserver/ConfiguarationServerApplication.java
+++ configuaration-server/src/main/java/yafiet/net/configuarationserver/ConfiguarationServerApplication.java
@@ -2,8 +2,10 @@ package yafiet.net.configuarationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfiguarationServerApplication {
 
 	public static void main(String[] args) {
