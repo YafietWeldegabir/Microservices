cb196c5a30530a458551eed381e6e408ad4d5567
YafietWeldegabir <yafietweldegabir@gmail.com>
2021-11-25 16:03:00 -0600

    discovery-server

---
+++ configuaration-server/src/main/java/yafiet/net/configuarationserver/ConfiguarationServerApplication.java
@@ -0,0 +1,13 @@
+package yafiet.net.configuarationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguarationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguarationServerApplication.class, args);
+	}
+
+}
---
+++ configuaration-server/src/test/java/yafiet/net/configuarationserver/ConfiguarationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package yafiet.net.configuarationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguarationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
